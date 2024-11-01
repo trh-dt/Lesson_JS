59b71173ef047d86986ad8923c5d768a9bc9b2a4
trh-dt <terekh.d08@gmal.com>
2024-11-01 16:44:39 +0200

    commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(nam1 , num2) {
-    return (nam1 , num2) / 2
+    return (10 , 20) / 2
 }
 
-let result = calculateAverage(10, 15);
+let result = calculateAverage(10, 20);
 console.log(result);
