993bdb257f61b9b4d749417f8df4cdae1b59e428
trh-dt <terekh.d08@gmal.com>
2024-11-01 17:03:23 +0200

    commit

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(nam1, nam2, nam3) {
+    return Math.max (nam1, nam2, nam3)
+}
+console.log (findLargest(12, 23, 35));
+
