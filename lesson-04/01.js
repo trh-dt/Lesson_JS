9fb0352ae9c603b6c2ef1fdd8740086e4b64f094
trh-dt <terekh.d08@gmal.com>
2024-11-13 10:30:58 +0200

    commit

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,14 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] === element) {
+            return true
+        }
+    }
+    return false;
+}
+console.log(includesElement([1, 2, 3], 2));
+console.log(includesElement([1, 2, 3], 4));
+
